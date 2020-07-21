b80d4d824ac88b0341b9d76c77150c3fbb0bcbe2
starsky77 <399680958@qq.com>
2020-07-21 16:25:33 +0800

    Add moveObj class
    Add moveObj class and some change of staticObj class

--- ZJUSandBox/TestProSDL_2/Entity.cpp
+++ ZJUSandBox/TestProSDL_2/Entity.cpp
@@ -6,73 +6,471 @@
 
 #include"Entity.h";
 
-const int Window_WIDTH = 640;
-const int Window_HEIGHT = 480;
-SDL_Window* window = NULL;
-SDL_Renderer* render = NULL;
-int Entity::GetPosX() {
-	return x;
-}
-int Entity::GetPosY() {
-	return y;
-}
-StaticObj::StaticObj(const char* file) {
-	imag = SDL_LoadBMP(file);
-	if (imag == NULL)
+
+const int LEVEL_WIDTH = 1280;
+const int LEVEL_HEIGHT = 960;
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+SDL_Window* gWindow = NULL;
+SDL_Renderer* gRenderer = NULL;
+LTexture gBGTexture;
+
+
+bool init()
+{
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		cout << "StaticObj:load imag error\n";
-		return;
+		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		//Set texture filtering to linear
+		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
+		{
+			printf("Warning: Linear texture filtering not enabled!");
+		}
+
+		//Create window
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			//Create vsynced renderer for window
+			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+			if (gRenderer == NULL)
+			{
+				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+				success = false;
+			}
+			else
+			{
+				//Initialize renderer color
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+				//Initialize PNG loading
+				int imgFlags = IMG_INIT_PNG;
+				if (!(IMG_Init(imgFlags) & imgFlags))
+				{
+					printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+					success = false;
+				}
+			}
+		}
+	}
+
+	return success;
+}
+
+void close()
+{
+	//Free loaded images
+
+	//Destroy window	
+	SDL_DestroyRenderer(gRenderer);
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+	gRenderer = NULL;
+
+	//Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
+}
+
+
+LTexture::LTexture()
+{
+	//Initialize
+	mTexture = NULL;
+	mWidth = 0;
+	mHeight = 0;
+}
+
+LTexture::~LTexture()
+{
+	//Deallocate
+	free();
+}
+
+bool LTexture::loadFromFile(std::string path)
+{
+	//Get rid of preexisting texture
+	free();
+
+	//The final texture
+	SDL_Texture* newTexture = NULL;
+
+	//Load image at specified path
+	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+	if (loadedSurface == NULL)
+	{
+		printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
+	}
+	else
+	{
+		//Color key image
+		SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0xFF, 0xFF, 0xFF));
+
+		//Create texture from surface pixels
+		newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+		if (newTexture == NULL)
+		{
+			printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+		}
+		else
+		{
+			//Get image dimensions
+			mWidth = loadedSurface->w;
+			mHeight = loadedSurface->h;
+		}
+
+		//Get rid of old loaded surface
+		SDL_FreeSurface(loadedSurface);
+	}
+
+	//Return success
+	mTexture = newTexture;
+	return mTexture != NULL;
+}
+
+void LTexture::free()
+{
+	//Free texture if it exists
+	if (mTexture != NULL)
+	{
+		SDL_DestroyTexture(mTexture);
+		mTexture = NULL;
+		mWidth = 0;
+		mHeight = 0;
+	}
+}
+
+void LTexture::setColor(Uint8 red, Uint8 green, Uint8 blue)
+{
+	//Modulate texture rgb
+	SDL_SetTextureColorMod(mTexture, red, green, blue);
+}
+
+void LTexture::setBlendMode(SDL_BlendMode blending)
+{
+	//Set blending function
+	SDL_SetTextureBlendMode(mTexture, blending);
+}
+
+void LTexture::setAlpha(Uint8 alpha)
+{
+	//Modulate texture alpha
+	SDL_SetTextureAlphaMod(mTexture, alpha);
+}
+
+void LTexture::render(int x, int y, SDL_Rect* clip)
+{
+	//Set rendering space and render to screen
+	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
+
+	//Set clip rendering dimensions
+	if (clip != NULL)
+	{
+		renderQuad.w = clip->w;
+		renderQuad.h = clip->h;
+	}
+
+	//Render to screen
+	SDL_RenderCopy(gRenderer, mTexture, clip, &renderQuad);
+}
+
+int LTexture::getWidth()
+{
+	return mWidth;
+}
+
+int LTexture::getHeight()
+{
+	return mHeight;
+}
+
+
+
+int Entity::GetPosX()
+{
+	return mPosX;
+}
+int Entity::GetPosY()
+{
+	return mPosY;
+}
+int Entity::GetW()
+{
+	return w;
+}
+int Entity::GetH()
+{
+	return h;
+}
+
+StaticObj::StaticObj(const char* file)
+{
+
+	if (!StaticObjTex.loadFromFile(file))
+	{
+		printf("Failed to load the texture!\n");
 	}
 	rect.x = 0;
 	rect.y = 0;
-	rect.h = imag->h;
-	rect.w = imag->w;
-	tex = SDL_CreateTextureFromSurface(render, imag);
-	//SDL_FreeSurface(imag);
+	rect.h = StaticObjTex.getHeight();
+	rect.w = StaticObjTex.getWidth();
 	return;
 }
-void StaticObj::Render(int camX, int camY, int size) {
+
+void StaticObj::SetPos(int x, int y)
+{
+	mPosX = x;
+	mPosY = y;
+}
+
+void StaticObj::Render(int camX, int camY)
+{
 	rect.x = camX;
 	rect.y = camY;
-	rect.h = rect.h / size;
-	rect.w = rect.w / size;
-	bool quit = false;
-	SDL_Event event;
-	while (!quit)
-	{
-		while (SDL_PollEvent(&event) != 0)
-		{
-			if (event.type == SDL_QUIT)
-			{
-				quit = true;
-				cout << "quit\n";
-			}
-		}
-		SDL_RenderCopy(render, tex, NULL, &rect);
-		SDL_RenderPresent(render);
-		SDL_Delay(1000);
-	}
+
+	StaticObjTex.render(mPosX - camX, mPosY - camY);
 }
+
 StaticObj::~StaticObj() {
 	SDL_FreeSurface(imag);
 	SDL_DestroyTexture(tex);
 }
 
 
+MoveObj::MoveObj(const char* file, int w, int h)
+{
+	mVelX = 0;
+	mVelY = 0;
+	mPosX = 0;
+	mPosY = 0;
+	frame = 0;
+	this->w = w;
+	this->h = h;   //for the test piecture,w should be 32,h should be 48
+	//Load sprite sheet texture
+	if (!gSpriteSheetTexture.loadFromFile(file))
+	{
+		printf("Failed to load the texture!\n");
+	}
+	else
+	{
+		for (int j = KEY_PRESS_SURFACE_DOWN; j < KEY_PRESS_SURFACE_TOTAL; j++)
+		{
+			for (int i = 0; i < WALKING_ANIMATION_FRAMES; i++)
+			{
+				gSpriteClips[j][i].x = w * i;
+				gSpriteClips[j][i].y = h * j;
+				gSpriteClips[j][i].w = w;
+				gSpriteClips[j][i].h = h;
+
+			}
+		}
+
+	}
+	isMoving = false;
+	curDirection = KEY_PRESS_SURFACE_DOWN;
+}
+
+MoveObj::~MoveObj()
+{
+	gSpriteSheetTexture.free();
+}
+
+int MoveObj::GetVelX()
+{
+	return mVelX;
+}
+int MoveObj::GetVelY()
+{
+	return mVelY;
+}
+
+
+void MoveObj::handleEvent(SDL_Event& e)
+{
+	//If a key was pressed
+	if (e.type == SDL_KEYDOWN && e.key.repeat == 0)
+	{
+		isMoving = true;
+		//Adjust the velocity
+		switch (e.key.keysym.sym)
+		{
+		case SDLK_UP:
+			mVelY -= DOT_VEL;
+			curDirection = KEY_PRESS_SURFACE_UP;
+			break;
+		case SDLK_DOWN:
+			mVelY += DOT_VEL;
+			curDirection = KEY_PRESS_SURFACE_DOWN;
+			break;
+		case SDLK_LEFT:
+			mVelX -= DOT_VEL;
+			curDirection = KEY_PRESS_SURFACE_LEFT;
+			break;
+		case SDLK_RIGHT:
+			mVelX += DOT_VEL;
+			curDirection = KEY_PRESS_SURFACE_RIGHT;
+			break;
+		}
+	}
+	//If a key was released
+	else if (e.type == SDL_KEYUP && e.key.repeat == 0)
+	{
+		isMoving = false;
+		//Adjust the velocity
+		switch (e.key.keysym.sym)
+		{
+		case SDLK_UP: mVelY += DOT_VEL; break;
+		case SDLK_DOWN: mVelY -= DOT_VEL; break;
+		case SDLK_LEFT: mVelX += DOT_VEL; break;
+		case SDLK_RIGHT: mVelX -= DOT_VEL; break;
+		}
+	}
+}
+
+void MoveObj::Move()
+{
+	//Move the dot left or right
+	mPosX += mVelX;
+
+	//If the dot went too far to the left or right
+	if ((mPosX < 0) || (mPosX + w > LEVEL_WIDTH))
+	{
+		//Move back
+		mPosX -= mVelX;
+	}
+
+	//Move the dot up or down
+	mPosY += mVelY;
+
+	//If the dot went too far up or down
+	if ((mPosY < 0) || (mPosY + h > LEVEL_HEIGHT))
+	{
+		//Move back
+		mPosY -= mVelY;
+	}
+}
+
+void MoveObj::Render(int camX, int camY)
+{
+	if (isMoving)
+	{
+		frame++;
+		if (frame / MoveSpeedFrame >= WALKING_ANIMATION_FRAMES)
+		{
+			frame = 0;
+		}
+	}
+	else
+	{
+		frame = 0;
+	}
+
+	gSpriteSheetTexture.render(mPosX - camX, mPosY - camY, &gSpriteClips[curDirection][frame / MoveSpeedFrame]);
+	//SDL_RenderPresent(gRenderer);
+
+}
+
+int MoveObj::GetMaxFrames()
+{
+	return WALKING_ANIMATION_FRAMES;
+}
+
 
 
 
 
 int main(int argc, char* args[]) {
+
+
+	/*
 	SDL_Init(SDL_INIT_EVERYTHING);
-	window = SDL_CreateWindow("test1", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, Window_WIDTH, Window_HEIGHT, SDL_WINDOW_SHOWN);
-	render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	gWindow = SDL_CreateWindow("test1", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, Window_WIDTH, Window_HEIGHT, SDL_WINDOW_SHOWN);
+	gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	StaticObj t1("img/hello.bmp");
 	t1.Render(100, 100, 1);
-	SDL_DestroyWindow(window);
-	SDL_DestroyRenderer(render);
+	SDL_Delay(1000);
+	SDL_DestroyWindow(gWindow);
+	SDL_DestroyRenderer(gRenderer);
 	SDL_Quit();
 	system("pause");
 	return 0;
+	*/
+	if (!init())
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		MoveObj testObj("img/man_1.png", 32, 48);
+		StaticObj t1("img/hello_world.bmp");
+		gBGTexture.loadFromFile("img/testBG.png");
+		bool quit = false;
 
+		//Event handler
+		SDL_Event e;
+
+		SDL_Rect camera = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
+
+		while (!quit)
+		{
+			//Handle events on queue
+			while (SDL_PollEvent(&e) != 0)
+			{
+				//User requests quit
+				if (e.type == SDL_QUIT)
+				{
+					quit = true;
+				}
+				testObj.handleEvent(e);
+			}
+			testObj.Move();
+
+			camera.x = (testObj.GetPosX() + testObj.GetW() / 2) - SCREEN_WIDTH / 2;
+			camera.y = (testObj.GetPosY() + testObj.GetH() / 2) - SCREEN_HEIGHT / 2;
+
+			//Keep the camera in bounds
+			if (camera.x < 0)
+			{
+				camera.x = 0;
+			}
+			if (camera.y < 0)
+			{
+				camera.y = 0;
+			}
+			if (camera.x > LEVEL_WIDTH - camera.w)
+			{
+				camera.x = LEVEL_WIDTH - camera.w;
+			}
+			if (camera.y > LEVEL_HEIGHT - camera.h)
+			{
+				camera.y = LEVEL_HEIGHT - camera.h;
+			}
+
+			//Clear screen
+			SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+			SDL_RenderClear(gRenderer);
+
+			gBGTexture.render(0, 0, &camera);
+			t1.Render(camera.x, camera.y);
+			testObj.Render(camera.x, camera.y);
+			SDL_RenderPresent(gRenderer);
+
+		}
+
+	}
+	close();
+	return 0;
 }
--- ZJUSandBox/TestProSDL_2/Entity.h
+++ ZJUSandBox/TestProSDL_2/Entity.h
@@ -9,58 +9,134 @@
 
 using namespace std;
 
+enum KeyPressSurfaces
+{
+	KEY_PRESS_SURFACE_DOWN,
+	KEY_PRESS_SURFACE_LEFT,
+	KEY_PRESS_SURFACE_RIGHT,
+	KEY_PRESS_SURFACE_UP,
+	KEY_PRESS_SURFACE_TOTAL
+};
+
+//Texture wrapper class
+class LTexture
+{
+public:
+	//Initializes variables
+	LTexture();
+
+	//Deallocates memory
+	~LTexture();
+
+	//Loads image at specified path
+	bool loadFromFile(std::string path);
+
+	//Deallocates texture
+	void free();
+
+	//Set color modulation
+	void setColor(Uint8 red, Uint8 green, Uint8 blue);
+
+	//Set blending
+	void setBlendMode(SDL_BlendMode blending);
+
+	//Set alpha modulation
+	void setAlpha(Uint8 alpha);
+
+	//Renders texture at given point
+	void render(int x, int y, SDL_Rect* clip = NULL);
+
+	//Gets image dimensions
+	int getWidth();
+	int getHeight();
+
+private:
+	//The actual hardware texture
+	SDL_Texture* mTexture;
+
+	//Image dimensions
+	int mWidth;
+	int mHeight;
+};
+
+
+
 
 class Entity
 {
 public:
-	virtual void Render(int camX, int camY, int size = 1) = 0;
+	//virtual void Render(int camX, int camY, int size = 1) = 0;
 
 	//Position accessors
-	int GetPosX();
-	int GetPosY();
-
+	virtual int GetPosX();
+	virtual int GetPosY();
+	virtual int GetW();
+	virtual int GetH();
 
 protected:
 	//The X and Y offsets of the dot
-	int mPosX, mPosY;
-	//if need to draw
-	int x, y;//the location in the windows
+	int mPosX, mPosY;//the location in the windows
 	int w, h;//height,width
 
 };
 
 
-class MoveObj :Entity
+class StaticObj :public Entity
 {
 public:
-	virtual void Move();
-	virtual void Render(int camX, int camY);
-	MoveObj();
-	~MoveObj();
-	int GetVelX();
-	int GetVelY();
-
-
-
-protected:
-	//The velocity of the dot
-	int mVelX, mVelY;
-
-};
-
-class StaticObj :Entity
-{
-public:
-	void Render(int camX, int camY, int size = 1);
+	void Render(int camX, int camY);
+	void SetPos(int x, int y);
 	StaticObj(const char* file);
 	~StaticObj();
 
 private:
+	LTexture StaticObjTex;
 	SDL_Rect rect;
 	SDL_Surface* imag = NULL;
 	SDL_Texture* tex = NULL;
 };
 
+
+class MoveObj :public Entity
+{
+public:
+	MoveObj(const char* file, int w, int h);
+	~MoveObj();
+
+	virtual void Move();
+	virtual void Render(int camX, int camY);
+	void handleEvent(SDL_Event& e);
+
+	int frame;
+
+	virtual int GetVelX();
+	virtual int GetVelY();
+	virtual int GetMaxFrames();
+
+
+
+protected:
+	//The total number of pictures in a set of actions
+	static const int  WALKING_ANIMATION_FRAMES = 4;
+	//Number of pixels moved per time
+	static const int DOT_VEL = 4;
+	//The number of frames interval between each switching action
+	static const int MoveSpeedFrame = 12;
+
+	//The velocity of the Obj
+	int mVelX, mVelY;
+
+	//Store image files
+	SDL_Rect  gSpriteClips[KEY_PRESS_SURFACE_TOTAL][WALKING_ANIMATION_FRAMES];
+	LTexture gSpriteSheetTexture;
+
+	//Indicates the current direction
+	KeyPressSurfaces curDirection;
+	bool isMoving;
+
+};
+
+/*
 class Role :MoveObj
 {
 public:
@@ -83,6 +159,6 @@ public:
 private:
 
 };
-
+*/
 
 #endif // !1
